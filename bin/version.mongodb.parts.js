9b1cf8b8a8df4400a1025a3de1350d52f3050405
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-11-17 13:28:13 -0500

    handle dynamic heroku port assignment

--- bin/version.mongodb.parts.js
+++ bin/version.mongodb.parts.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 var server = require(__dirname + '/../');
-console.log('ENV', process.env);
 server.listen(server.config.get('port'), server.config.get('hostname'), function(){
   console.log('version.mongodb.parts: %s', server.config.get('listen'));
 });
