bb78c8ccd7533b7bb5b55d51431aae0ea2797fbd
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2014-11-17 16:10:40 -0500

    get rid of header hack and use more HATEOS 302

--- test/index.test.js
+++ test/index.test.js
@@ -2,27 +2,26 @@ var app = require('../'),
   request = require('supertest'),
   assert = require('assert');
 
-function GET(path, done){
-  request(app).get(path).end(done);
-}
-
 describe('version.mongodb.parts', function(){
   it('Where can I download the latest stable version of MongoDB for OSX?', function(done){
-    GET('/download/stable?platform=osx', function(err, res){
-      if(err) return done(err);
+    request(app)
+      .get('/download/stable?platform=osx')
+      .end(function(err, res){
+        if(err) return done(err);
 
-      assert.equal(res.headers.location.indexOf('http://fastdl.mongodb.org/osx/mongodb-osx-x86_64-'), 0);
-      assert(/\d+\.[68]+\.\d+/.test(res.headers['mongodb-version']));
-      done(err);
-    });
+        assert(/\/download\/\d+\.[68]+\.\d+/.test(res.headers.location));
+        done(err);
+      });
   });
 
   it('Do you know where I might be able to download the 2.8 rc0 for ubuntu 64-bit?', function(done){
-    GET('/download/unstable?platform=linux&bits=64', function(err, res){
-      if(err) return done(err);
-      assert.equal(res.headers.location.indexOf('http://fastdl.mongodb.org/linux/mongodb-linux-x86_64'), 0);
-      assert(/\d+\.[89]+\.\d+(:-rc\d+)?/.test(res.headers['mongodb-version']));
-      done(err);
-    });
+    request(app)
+      .get('/download/unstable?platform=linux&bits=64')
+      .end(function(err, res){
+        if(err) return done(err);
+
+        assert(/\/download\/\d+\.[89]+\.\d+(:-rc\d+)?/.test(res.headers.location));
+        done(err);
+      });
   });
 });
